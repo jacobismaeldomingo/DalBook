676bcba49f6b8e21daf764207607bc1b78bb5039
Jacob Domingo <jc298378@dal.ca>
2024-06-07 17:00:04 -0300

    feat: added spring boot project

---
+++ backend/facebook-integration/src/test/java/com/example/facebook_integration/FacebookIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facebook_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
