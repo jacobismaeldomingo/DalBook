08a4ea75c8272630e7a1168fc1ac55cfb93b3812
Jay Sanghani <sanghanijay23@gmail.com>
2024-06-12 22:14:19 -0300

    build: Form.js updated with basic code

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -1,21 +1,47 @@
 import { useState } from 'react';
 
 function Form(){
-    const [name, setName] = useState('');
 
-  const handleChange = (event) => {
-    setName(event.target.value);
-  }
+        const [formData, setFormData] = useState({
+            firstname:'', 
+            lastname: '',
+            email: '',
+            password:'',
+        });
 
-  return (
-    <form>
-      <label>
-        Enter your name:
-        <input type="text" value={name} onChange={handleChange} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+        const handleChange = (event) => {
+            setFormData({[event.target.name]: event.target.value, }); 
+            console.log(formData.name); // Log the updated state
+        };
+
+    return (
+        <div>
+            <h1>Registration Page</h1>
+        <form>
+        <label>
+            Enter your First Name:
+            <input type="text"  onChange={handleChange} />
+        </label><br />
+        <label>
+            Enter your Last Name:
+            <input type="text"  onChange={handleChange} />
+        </label><br />
+        <label>
+            Enter your Dal Email:
+            <input type="email"  onChange={handleChange} />
+        </label><br />
+        <label>
+            Set your password:
+            <input type="password"  onChange={handleChange} />
+        </label><br />
+        <button type="submit">Submit</button>
+        </form>
+        <p> fname:{formData.firstname}<br />
+            lname:{}<br />
+            email:{}<br />
+            password:{}
+            </p>
+        </div>
+    );
 }
-
 export default Form;
