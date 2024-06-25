95e85cdf4c0661298c792c20739e433071913bec
Jacob Ismael Domingo <jc298378@dal.ca>
2024-06-25 13:36:53 -0300

    Revert "Merge branch 'feature/issue-7' into 'main'"
    This reverts merge request !21

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
