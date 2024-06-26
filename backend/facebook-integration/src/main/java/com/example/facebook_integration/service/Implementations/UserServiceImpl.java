e2ab60bcef92462d46ebc353a484f44362f8ee41
Jacob Domingo <jc298378@dal.ca>
2024-06-25 21:11:46 -0300

    feat: updated User backend

---
+++ backend/facebook-integration/src/main/java/com/example/facebook_integration/service/Implementations/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.facebook_integration.service.Implementations;
+
+import com.example.facebook_integration.model.User;
+import com.example.facebook_integration.repository.UserRepository;
+import com.example.facebook_integration.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    // Method to create a new resume in the database
+    @Override
+    public String createUser(User user) {
+        // Validation check if resume is null, throw exception
+        if (user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        userRepository.save(user);
+        return "User created successfully";
+    }
+}
