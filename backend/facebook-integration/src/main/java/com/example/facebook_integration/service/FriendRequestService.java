cffd23246ee24f5525e24c47b4844ccc37b76465
Jacob Domingo <jc298378@dal.ca>
2024-06-26 21:34:29 -0300

    feat: added Friend Request feature (hardcoded user id)

---
+++ backend/facebook-integration/src/main/java/com/example/facebook_integration/service/FriendRequestService.java
@@ -0,0 +1,16 @@
+package com.example.facebook_integration.service;
+
+import com.example.facebook_integration.model.FriendRequest;
+import com.example.facebook_integration.model.User;
+
+import java.util.List;
+
+public interface FriendRequestService {
+    FriendRequest sendRequest(int senderId, int receiverId);
+
+    FriendRequest acceptRequest(int requestId);
+
+    List<FriendRequest> getPendingRequests(int userId);
+
+    List<User> getFriends(int userId);
+}
