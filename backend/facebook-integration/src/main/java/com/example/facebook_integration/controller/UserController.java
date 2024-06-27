3ed409a48ac1d2e8195c53dd39b9dd80db3f7098
Jacob Domingo <jc298378@dal.ca>
2024-06-27 00:34:58 -0300

    feat: fixing some issues

--- backend/facebook-integration/src/main/java/com/example/facebook_integration/controller/UserController.java
+++ backend/facebook-integration/src/main/java/com/example/facebook_integration/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.facebook_integration.controller;
 import com.example.facebook_integration.model.User;
 import com.example.facebook_integration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
