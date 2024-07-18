3cf53f1a0764a39029770b1474ed522a043613ba
Junjie Li <jn281687@dal.ca>
2024-07-18 14:41:37 -0300

    added new friend notification

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,7 +1,8 @@
 // Home Page
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
+import friendService from "../../services/FriendService";
 import {
   IconSearch,
   IconHome,
@@ -15,6 +16,23 @@ import {
 
 function Header() {
   const navigate = useNavigate();
+
+  const [pendingRequests, setPendingRequests] = useState(0);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    if (storedUserId) {
+      friendService
+        .getPendingRequests(storedUserId)
+        .then((response) => {
+          setPendingRequests(response.data.length);
+        })
+        .catch((error) => {
+          console.error("Error fetching pending requests:", error);
+          alert("An error occurred. Please try again!");
+        });
+    }
+  }, []);
 
   const homePage = () => {
     navigate("/home");
@@ -34,6 +52,7 @@ function Header() {
   const handleFriendRequestsList = () => {
     navigate("/FriendRequestList");
   };
+  
 
   return (
     <div className="homepage">
@@ -65,7 +84,7 @@ function Header() {
             <IconBuildingStore stroke={2} size={30} color="#1877F2" />
           </div>
           <div className="icon" onClick={handleFriendRequests}>
-            <IconUsersGroup stroke={2} size={30} color="#1877F2" />
+            <IconUsersGroup stroke={2} size={30} color="#1877f2" />
           </div>
         </div>
         <div className="account-header">
@@ -75,8 +94,19 @@ function Header() {
           <div className="messages">
             <IconMessages stroke={2} size={30} color="#1877F2" />
           </div>
-          <div className="notifications" onClick={handleFriendRequestsList}>
-            <IconBell stroke={2} size={30} color="#1877F2" />
+          <div className="notifications" onClick={handleFriendRequestsList} style={{ position: 'relative' }}>
+            <IconBell stroke={2} size={30} color="#1877f2" />
+            {pendingRequests > 0 && (
+              <span style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                width: '10px',
+                height: '10px',
+                backgroundColor: 'red',
+                borderRadius: '50%',
+              }}></span>
+            )}
           </div>
           <div
             className="profile-header"
