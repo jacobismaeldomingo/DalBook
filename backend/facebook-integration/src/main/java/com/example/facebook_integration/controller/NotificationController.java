dcea56f2c6e9aedd8221b776c6e00de2565c4132
Jacob Domingo <jc298378@dal.ca>
2024-07-27 01:23:08 -0300

    chore: fixing build issue on backend

--- backend/facebook-integration/src/main/java/com/example/facebook_integration/controller/NotificationController.java
+++ backend/facebook-integration/src/main/java/com/example/facebook_integration/controller/NotificationController.java
@@ -2,8 +2,6 @@ package com.example.facebook_integration.controller;
 
 import com.example.facebook_integration.model.FriendRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
